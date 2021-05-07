637137d24130491be1819181eee5e9d4f78b8c41
Nischal Poudel <nschalpdl32@gmail.com>
2021-05-07 13:29:59 -0400

    my pages edit

--- server/index.js
+++ server/index.js
@@ -1,31 +1,33 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 var routes = require("./routes.js");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /* ---------------------------------------------------------------- */
 /* ------------------- Route handler registration ----------------- */
 /* ---------------------------------------------------------------- */
 
 // input routes for application
-app.post('/check_login', routes.check_login)
+app.post("/check_login", routes.check_login);
 
 // routes for creating user
-app.post('/create_user', routes.user_signup)
+app.post("/create_user", routes.user_signup);
 
 //Acount Page
-app.get('/account/:sessionID', routes.getAccountPage);
-
+app.get("/account/:sessionID", routes.getAccountPage);
 
 //Search Page
-app.get('/searchPage/:song_title', routes.getSongFromDB);
+app.get("/searchPage/:song_title", routes.getSongFromDB);
+
+//Search Page
+app.get("/userMoodDistro/:email", routes.getUserMoodDistro);
 
 app.listen(8081, () => {
-	console.log(`Server listening on PORT 8081`);
+    console.log(`Server listening on PORT 8081`);
 });
--- server/routes.js
+++ server/routes.js
@@ -1,18 +1,15 @@
-const config = require('./db-config.js')
-const mysql = require('mysql')
+const config = require("./db-config.js");
+const mysql = require("mysql");
 
-config.connectionLimit = 10
-const connection = mysql.createPool(config)
-
+config.connectionLimit = 10;
+const connection = mysql.createPool(config);
 
 // for password hashing
-var crypto = require('crypto');
-
+var crypto = require("crypto");
 
 /* -------------------------------------------------- */
 /* ------------------- Route Handlers --------------- */
 /* -------------------------------------------------- */
-
 
 const checkLogin = (req, res) => {
     // grab username, password from the frontend form
@@ -20,93 +17,130 @@ const checkLogin = (req, res) => {
     var password = req.body.password;
 
     // compute hashed password
-    var hashed_password = crypto.createHash("sha256").update(password).digest("hex");
+    var hashed_password = crypto
+        .createHash("sha256")
+        .update(password)
+        .digest("hex");
 
     // query database to check if user already exists
-    const query = `SELECT * FROM Users WHERE email = '${username}' AND (password = '${hashed_password}' OR password = '${password}')`
+    const query = `SELECT * FROM Users WHERE email = '${username}' AND (password = '${hashed_password}' OR password = '${password}')`;
 
     connection.query(query, (err, rows, fields) => {
-        if (err) console.log(err)
+        if (err) console.log(err);
         else {
-            console.log(rows)
-            if (rows.length == 1) res.send(true)
-            else res.send(false)
+            console.log(rows);
+            if (rows.length == 1) res.send(true);
+            else res.send(false);
         }
-    })
-}
-
+    });
+};
 
 // TODO: implement signup
 const userSignup = (req, res) => {
     // grab username, password from frontend form
     var username = req.body.email;
-    var password = req.body.password
+    var password = req.body.password;
 
     // hash password
-    var hashed_password = crypto.createHash("sha256").update(password).digest("hex");
-	console.log("hashed Password: ", hashed_password);
+    var hashed_password = crypto
+        .createHash("sha256")
+        .update(password)
+        .digest("hex");
+    console.log("hashed Password: ", hashed_password);
 
     // make call to check if user already exists
-    const query = `SELECT * FROM Users WHERE email = '${username}' AND password = '${hashed_password}'`
+    const query = `SELECT * FROM Users WHERE email = '${username}' AND password = '${hashed_password}'`;
 
     connection.query(query, (err, rows, fields) => {
-        if (err) console.log(err)
+        if (err) console.log(err);
         else {
-            console.log(rows)
-            console.log("check user called!")
+            console.log(rows);
+            console.log("check user called!");
 
             if (rows.length == 0) {
                 // insert into database with username, hashed password
-                const insert_query = `INSERT INTO Users (email, password) VALUES ('${username}', '${hashed_password}')`
+                const insert_query = `INSERT INTO Users (email, password) VALUES ('${username}', '${hashed_password}')`;
                 connection.query(insert_query, (err, rows, fields) => {
-                    if (err) throw err
+                    if (err) throw err;
                     else {
-                        console.log("Success! User with email " + username  + " inserted!")
+                        console.log(
+                            "Success! User with email " +
+                                username +
+                                " inserted!"
+                        );
 
-                        res.send(true)
+                        res.send(true);
                     }
-                })
+                });
             } else {
-                console.log("User already exists")
+                console.log("User already exists");
             }
         }
-    })
-}
+    });
+};
 
 //Account Page based on Session ID (TODO: needs to be generated on login)
 const getAccountPage = (req, res) => {
-
     //TODO, retrieve information based on Session ID
     //(Maybe need to make a new db table for this)
 
     connection.query(query, (err, rows, fields) => {
-      if (err) console.log(err);
-      else {res.json(rows)};
+        if (err) console.log(err);
+        else {
+            res.json(rows);
+        }
     });
 };
 
 //Search Page based on keyword)
 const getSongFromDB = (req, res) => {
-
-    const query = `
+    const query =
+        `
     SELECT s.title, s.artist, s.mood, s.release_year, s.popularity
     FROM Songs AS s
-    WHERE s.title LIKE "`+ req.params.song_title + `"
+    WHERE s.title LIKE "` +
+        req.params.song_title +
+        `"
     LIMIT 10;
     `;
 
     console.log("Sent Query with: " + req.params.song_title);
     connection.query(query, (err, rows, fields) => {
-      if (err) console.log(err);
-      else {console.log("Got Response:" + rows); res.json(rows);}
+        if (err) console.log(err);
+        else {
+            console.log("Got Response:" + rows);
+            res.json(rows);
+        }
     });
 };
 
+// Get the mood distribution of songs in user's saved songs
+const getUserMoodDistro = (req, res) => {
+    const user_email = req.params.email;
 
+    const query = `
+    SELECT count(ss.song_id) as num_songs, CEIL((mm.mood * 10)) AS mood_bucket
+    FROM SavedSongs ss 
+    JOIN Songs s ON s.spotify_id = ss.song_id 
+    JOIN MoodMetrics mm ON mm.song_id = ss.song_id 
+    WHERE ss.email = "${user_email}"
+    GROUP BY mood_bucket
+    ORDER BY mood_bucket ;
+    `;
+
+    console.log("User Mood Query with: " + user_email);
+    connection.query(query, (err, rows, fields) => {
+        if (err) console.log(err);
+        else {
+            res.json(rows);
+        }
+    });
+};
 
 module.exports = {
-    check_login : checkLogin,
-    user_signup : userSignup,
-    getAccountPage : getAccountPage,
-    getSongFromDB : getSongFromDB
-}
+    check_login: checkLogin,
+    user_signup: userSignup,
+    getAccountPage: getAccountPage,
+    getSongFromDB: getSongFromDB,
+    getUserMoodDistro: getUserMoodDistro,
+};
