392e7b919f98149223786cf42d1fdb3727bfc17a
Nischal Poudel <nschalpdl32@gmail.com>
2021-05-06 22:23:21 -0400

    updates to app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,8 @@ import {
 import Login from '../components/login/Login'
 import Signup from '../components/signup/Signup'
 import SearchPage from '../components/searchPage/searchPage'
-
+import MyPage from '../components/mypage/MyPage'
+import ArtistsPage from '../components/artistspage/ArtistsPage'
 
 export default class App extends React.Component {
 
@@ -34,6 +35,20 @@ export default class App extends React.Component {
 							exact
 							path="/search"
 							render={() => <SearchPage />}
+						/>
+					</Switch>
+                    <Switch>
+						<Route
+							exact
+							path="/mypage"
+							render={() => <MyPage />}
+						/>
+					</Switch>
+                    <Switch>
+						<Route
+							exact
+							path="/artistspage"
+							render={() => <ArtistsPage />}
 						/>
 					</Switch>
 				</Router>
