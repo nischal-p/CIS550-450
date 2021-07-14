d172ce5c74197ab0705650813c5c722dcbb05553
Chris Williams <csungwilliams@gmail.com>
2021-07-14 16:36:25 -0400

    Delete db-config.js

--- server/db-config.js
+++
@@ -1,8 +0,0 @@
-// Private, for accessing database (DO NOT COMMIT TO GIT)
-module.exports = {
-  host: "3.238.131.147",
-  port: "3306",
-  user: "test_remote",
-  password: "CIS550",
-  database: "project_data"
-};
